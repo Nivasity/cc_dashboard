3fafe47a7f060d642fa4f0cdaa4f290fd4e4be69
Samuel Akinyemi <akinyemisamuel170@gmail.com>
2024-02-17 22:25:00 +0100

    insert partials

---
+++ partials/_footer.php
@@ -0,0 +1,14 @@
+<footer class="content-footer footer bg-footer-theme">
+  <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
+    <div class="mb-2 mb-md-0">
+      ©
+      <script>
+        document.write(new Date().getFullYear());
+      </script>
+      Nivasity Web Services, all rights reserved.
+    </div>
+    <div>
+      made with ❤️ by <a href="https://themeselection.com" class="footer-link fw-bolder">ThemeSelection</a>
+    </div>
+  </div>
+</footer>
---
+++ partials/_sidebar.php
@@ -0,0 +1,137 @@
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+  <div class="app-brand">
+    <a href="/" class="app-brand-link">
+      <img class="img-fluid app-brand-logo " src="assets/img/nivasity_cc_logo_exp.png">
+    </a>
+
+    <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+      <i class="bx bx-chevron-left bx-sm align-middle"></i>
+    </a>
+  </div>
+
+  <div class="menu-inner-shadow"></div>
+
+  <ul class="menu-inner py-1">
+    <!-- Dashboard -->
+    <li class="menu-item active">
+      <a href="/" class="menu-link">
+        <i class="menu-icon tf-icons bx bx-home-circle"></i>
+        <div data-i18n="Analytics">Dashboard</div>
+      </a>
+    </li>
+    <!-- My Profile -->
+    <li class="menu-item">
+      <a href="profile.html" class="menu-link">
+        <i class="menu-icon tf-icons bx bx-user-circle"></i>
+        <div data-i18n="Analytics">My Profile</div>
+      </a>
+    </li>
+
+    <!-- Components -->
+    <li class="menu-header small text-uppercase"><span class="menu-header-text">Admin Management</span></li>
+    <li class="menu-item">
+      <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <i class="menu-icon tf-icons bx bx-group"></i>
+        <div data-i18n="Authentications">Users</div>
+      </a>
+      <ul class="menu-sub">
+        <li class="menu-item">
+          <a href="auth-login-basic.html" class="menu-link">
+            <div data-i18n="Basic">Profiles</div>
+          </a>
+        </li>
+        <li class="menu-item">
+          <a href="auth-login-basic.html" class="menu-link">
+            <div data-i18n="Basic">Roles</div>
+          </a>
+        </li>
+      </ul>
+    </li>
+    <li class="menu-item">
+      <a href="tables-basic.html" class="menu-link">
+        <i class="menu-icon tf-icons bx bx-key"></i>
+        <div data-i18n="Tables">Sign Up Keys</div>
+      </a>
+    </li>
+
+    <!-- Components -->
+    <li class="menu-header small text-uppercase"><span class="menu-header-text">Customer Management</span></li>
+    <li class="menu-item">
+      <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <i class="menu-icon tf-icons bx bx-building-house"></i>
+        <div data-i18n="Authentications">Schools</div>
+      </a>
+      <ul class="menu-sub">
+        <li class="menu-item">
+          <a href="auth-login-basic.html" class="menu-link">
+            <div data-i18n="Basic">School List</div>
+          </a>
+        </li>
+        <li class="menu-item">
+          <a href="auth-register-basic.html" class="menu-link">
+            <div data-i18n="Basic">Departments</div>
+          </a>
+        </li>
+      </ul>
+    </li>
+    <li class="menu-item">
+      <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <i class="menu-icon tf-icons bx bx-user-pin"></i>
+        <div data-i18n="Authentications">Students</div>
+      </a>
+      <ul class="menu-sub">
+        <li class="menu-item">
+          <a href="students.html" class="menu-link">
+            <div data-i18n="Basic">Student Profile</div>
+          </a>
+        </li>
+        <li class="menu-item">
+          <a href="students.html" class="menu-link">
+            <div data-i18n="Basic">Verify Student</div>
+          </a>
+        </li>
+        <li class="menu-item">
+          <a href="students.html" class="menu-link">
+            <div data-i18n="Basic">Email Students</div>
+          </a>
+        </li>
+      </ul>
+    </li>
+    <li class="menu-item">
+      <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <i class="menu-icon tf-icons bx bx-support"></i>
+        <div data-i18n="Misc">Support Tickets</div>
+      </a>
+      <ul class="menu-sub">
+        <li class="menu-item">
+          <a href="pages-misc-error.html" class="menu-link">
+            <div data-i18n="Error">Opened Tickets</div>
+          </a>
+        </li>
+        <li class="menu-item">
+          <a href="pages-misc-under-maintenance.html" class="menu-link">
+            <div data-i18n="Under Maintenance">Closed Tickets</div>
+          </a>
+        </li>
+      </ul>
+    </li>
+
+    <!-- Components -->
+    <li class="menu-header small text-uppercase"><span class="menu-header-text">Financial report</span></li>
+    <li class="menu-item">
+      <a href="tables-basic.html" class="menu-link">
+        <i class="menu-icon tf-icons bx bx-transfer"></i>
+        <div data-i18n="Tables">Transactions</div>
+      </a>
+    </li>
+
+    <!-- Misc -->
+    <li class="menu-header small text-uppercase"></li>
+    <li class="menu-item">
+      <a href="https://github.com/themeselection/sneat-html-admin-template-free/issues" class="menu-link">
+        <i class="menu-icon tf-icons bx bx-log-out"></i>
+        <div data-i18n="Support">Sign Out</div>
+      </a>
+    </li>
+  </ul>
+</aside>
