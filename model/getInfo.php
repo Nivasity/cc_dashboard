1b9027b35949dbec43c2b645f5d968c7f0753dd3
Samuel Akinyemi <akinyemisamuel170@gmail.com>
2024-02-19 18:35:20 +0100

    update

--- model/getInfo.php
+++ model/getInfo.php
@@ -72,6 +72,7 @@ $responseData = array(
 
 // Set the appropriate headers for JSON response
 header('Accept-Encoding: gzip, deflate');
+header('Cache-Control: no-store, no-cache, must-revalidate');
 header('Content-Type: application/json');
 
 // Encode the data as JSON and send it
