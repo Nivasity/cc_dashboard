f09d02a6c4232d581a01ef696608a5507612d342
Samuel Akinyemi <akinyemisamuel170@gmail.com>
2026-04-18 21:22:18 +0100

    feat(refunds): remove unnecessary inclusion of refund_engine.php

--- model/refunds.php
+++ model/refunds.php
@@ -2,7 +2,6 @@
 session_start();
 require_once(__DIR__ . '/config.php');
 require_once(__DIR__ . '/functions.php');
-require_once(__DIR__ . '/../../nivasity/model/refund_engine.php');
 
 header('Content-Type: application/json');
 
