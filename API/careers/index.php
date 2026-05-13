df4f51e6a3c56836924d5900e5ef09a7a1668881
Samuel Akinyemi <akinyemisamuel170@gmail.com>
2026-05-13 14:07:30 +0100

    feat(cors): add additional allowed origins for CORS headers

--- API/careers/index.php
+++ API/careers/index.php
@@ -495,6 +495,8 @@ function sendCareerCorsHeaders(): void
         'http://127.0.0.1:5173',
         'http://localhost:4173',
         'http://127.0.0.1:4173',
+        'http://localhost:8080',
+        'http://127.0.0.1:8080',
     ];
 
     if ($origin !== '' && in_array($origin, $allowedOrigins, true)) {
